d268188c38d7c60c6b2ba6a4be740e3347af55c7
Oksana.Pastukh <oksana.pastukh.itis.2020@lpnu.ua>
2020-11-02 11:16:30 +0200

    first

---
+++ Project4.1/project4.1.cpp
@@ -0,0 +1,51 @@
+// Lab_04_1.cpp
+// < Пастух Оксани >
+// Лабораторна робота № 4.1
+// Цикли .
+// Варіант 14
+
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main()
+{
+	int i, N;
+	double P;
+
+	cout << "N = "; cin >> N;
+
+	P = 1;
+	i = N;
+
+	while (i <= 10)
+	{
+		P *= ((double)i + (1 / (i * i)))/ (sqrt(1 + exp(1 / i)));
+		i++;
+	}
+	cout << P << endl;
+
+	P = 1;
+	i = N;
+	do {
+		P *= ((double)i + (1 / (i * i))) / (sqrt(1 + exp(1 / i)));
+		i++;
+	} while (i <= 10);
+	cout << P << endl;
+
+	P = 1;
+	for (i = N; i <= 10; i++)
+	{
+		P *= ((double)i + (1 / (i * i))) / (sqrt(1 + exp(1 / i)));
+	}
+	cout << P << endl;
+
+	P = 1;
+	for (i = 10; i >= N; i--)
+	{
+		P *= ((double)i + (1 / (i * i))) / (sqrt(1 + exp(1 / i)));
+	}
+	cout << P << endl;
+
+	return 0;
+}
